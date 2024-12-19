fixed bug for separating generation for masking and generation

T_perturb/Model/trainer.py
@@ -36,6 +36,7 @@ from T_perturb.src.metric import (
 from T_perturb.src.utils import (  # WarmupScheduler,;
     aggregate_attn_weights,
     compute_cos_similarity,
+    compute_rouge_score,
     modify_ckpt_state_dict,
     pearson,
     return_attn_weights,
@@ -803,67 +804,6 @@ class CountDecoderTrainer(LightningModule):
             val, val
         )  # Return mapped value, or original if not in dict
 
-    def compute_rouge_score(
-        self,
-        pred_ids: np.ndarray,
-        tgt_ids: np.ndarray,
-        rouge_len_list: list[int],
-        max_seq_length: int,
-        test_dict: dict[str, list],
-    ) -> dict[str, list[Any]]:
-        # Convert predictions and targets to object type for string replacement
-        pred_ids = pred_ids.astype(object)
-        tgt_ids = tgt_ids.astype(object)
-
-        # Exclude task token (assumed to be the first token)
-        pred_ids = pred_ids[:, 1:]
-
-        # Define special tokens to remove (e.g., 0, 1, 2, 3)
-        special_tokens = np.array([0, 1, 2, 3])
-        pred_ids[np.isin(pred_ids, special_tokens)] = ''
-        tgt_ids[np.isin(tgt_ids, special_tokens)] = ''
-
-        # Convert all integers to strings
-        pred_ids_ = pred_ids.astype(str)
-        tgt_ids_ = tgt_ids.astype(str)
-
-        for seq_len in rouge_len_list:
-            # Truncate sequences based on seq_len
-            if max_seq_length > seq_len:
-                pred_genes_short = pred_ids_[:, :seq_len]
-                true_genes_short = tgt_ids_[:, :seq_len]
-            else:
-                pred_genes_short = pred_ids_
-                true_genes_short = tgt_ids_
-
-            # Convert each row of token IDs to a string
-            pred_ids_str = np.apply_along_axis(
-                lambda row: ' '.join(row), axis=1, arr=pred_genes_short
-            )
-            tgt_ids_str = np.apply_along_axis(
-                lambda row: ' '.join(row), axis=1, arr=true_genes_short
-            )
-
-            # Remove extra spaces
-            pred_ids_str = np.array([' '.join(s.split()) for s in pred_ids_str])
-            tgt_ids_str = np.array([' '.join(s.split()) for s in tgt_ids_str])
-            print('pred_ids', pred_ids_str.tolist())
-            print('tgt_ids', tgt_ids_str.tolist())
-            # Compute ROUGE score for the entire batch at once
-            rouge_result = self.rouge.compute(
-                predictions=pred_ids_str.tolist(),
-                references=tgt_ids_str.tolist(),
-                rouge_types=['rouge1'],
-                use_aggregator=False,
-            )
-            print(rouge_result)
-            # Extract the per-sample ROUGE scores
-            per_sample_scores = rouge_result['rouge1']
-            # Store the results in the test_dict
-            test_dict[f'rouge1_{seq_len}'].extend(per_sample_scores)
-
-        return test_dict
-
     # def compute_rouge_score(
     #     self,
     #     pred_ids: np.ndarray,
@@ -1038,11 +978,12 @@ class CountDecoderTrainer(LightningModule):
             )
             # print(pred_ids_dict)
 
-            for time_step in pred_ids_dict.keys():
+            for t in self.pred_tps:
                 if self.return_rouge_score:
-                    pred_ids = pred_ids_dict[time_step].detach().cpu().numpy()
-                    tgt_ids = batch[time_step].detach().cpu().numpy()
-                    test_dict = self.compute_rouge_score(
+                    pred_ids = pred_ids_dict[t].detach().cpu().numpy()
+                    tgt_ids = batch[f'tgt_input_ids_t{t}'].detach().cpu().numpy()
+                    test_dict = compute_rouge_score(
+                        rouge=self.rouge,
                         pred_ids=pred_ids,
                         tgt_ids=tgt_ids,
                         rouge_len_list=self.rouge_seq_len_list,
@@ -1167,9 +1108,10 @@ class CountDecoderTrainer(LightningModule):
             if self.return_rouge_score:
                 if self.test_dict[f'rouge1_{self.rouge_seq_len_list[0]}']:
                     for seq_len in self.rouge_seq_len_list:
-                        metric_mean[f'rouge1_{seq_len}'] = np.mean(
+                        rouge_score = np.concatenate(
                             self.test_dict[f'rouge1_{seq_len}']
                         )
+                        metric_mean[f'rouge1_{seq_len}'] = np.mean(rouge_score, axis=0)
 
             metrics = pd.DataFrame(metric_mean, index=[0])
             # add metrics on the gene space

T_perturb/Modules/T_model.py
@@ -1083,8 +1083,8 @@ class CellGen(nn.Module):
                 - 'cls_embedding_t{t}': CLS token embeddings for time step t.
             - 'generate_id_dict': Dictionary of generated token ids.
         '''
-        generate_id_dict: Dict[str, torch.Tensor] = {}
-        # all_outputs: Dict[str, torch.Tensor] = {}
+        generate_id_dict: Dict[int, torch.Tensor] = {}
+        all_outputs: Dict[int, torch.Tensor] = {}
         if self.context_tps is not None:
             all_modelling_tps = self.pred_tps + self.context_tps
             all_modelling_tps = sorted(list(set(all_modelling_tps)))
@@ -1135,8 +1135,9 @@ class CellGen(nn.Module):
                 scores=scores,
                 tgt_time_step=time_step,
             )
-            generate_id_dict[tgt_input_id_key] = generated_ids
-        return outputs, generate_id_dict
+            generate_id_dict[time_step] = generated_ids
+            all_outputs[time_step] = outputs
+        return all_outputs, generate_id_dict
 
     def generate_sequence(
         self,
@@ -1271,7 +1272,7 @@ class CellGen(nn.Module):
             # add cls token to the ids and update scores and ids
             scores[:, 1:] = scores_
             tmp_ids[:, 1:] = tmp_ids_
-        return outputs, tmp_ids
+        return outputs[tgt_time_step], tmp_ids
 
 
 class CountHead(nn.Module):
@@ -1412,7 +1413,7 @@ class CountDecoder(nn.Module):
             not_masked=True,
         )
         count_outputs = {}
-        for _, t in enumerate(self.pred_tps):
+        for t in self.pred_tps:
             cls_embedding = outputs[t]['mean_embedding']
             count_outputs_tmp = self.count_decoder.forward(cls_embedding)
             count_outputs[f'count_output_t{t}'] = count_outputs_tmp
@@ -1443,10 +1444,11 @@ class CountDecoder(nn.Module):
         )
 
         count_outputs = {}
-        for t, output in outputs.items():
+        for t in self.pred_tps:
+            print(t)
             # cls_embedding = outputs['dec_embedding'][:, 0, :]
             count_outputs[f'count_output_t{t}'] = self.count_decoder.forward(
-                output[t]['mean_embedding']
+                outputs[t]['mean_embedding']
             )
-            count_outputs[f'cls_embedding_t{t}'] = output[t]['dec_embedding'][:, 0, :]
+            count_outputs[f'cls_embedding_t{t}'] = outputs[t]['dec_embedding'][:, 0, :]
         return count_outputs, generate_id_dict

T_perturb/Perturb/T_model.py
@@ -71,8 +71,8 @@ class PerturberMasking(CellGen):
             - 'count_output_t{t}': Count prediction for time step t.
             - 'cls_embedding_t{t}': CLS token embeddings for time step t.
         '''
-        generate_id_dict: Dict[str, torch.Tensor] = {}
-        # all_outputs: Dict[str, torch.Tensor] = {}
+        generate_id_dict: Dict[int, torch.Tensor] = {}
+        all_outputs: Dict[int, torch.Tensor] = {}
         if self.context_tps is not None:
             all_modelling_tps = self.pred_tps + self.context_tps
             all_modelling_tps = sorted(list(set(all_modelling_tps)))
@@ -86,6 +86,7 @@ class PerturberMasking(CellGen):
             if int(k.split('_t')[-1]) in all_modelling_tps
         }
         for time_step in self.pred_tps:
+            print('pred_tps', self.pred_tps)
             tgt_input_id_dict_ = {k: v.clone() for k, v in tgt_input_id_dict.items()}
             tgt_pad_dict_ = {k: v.clone() for k, v in tgt_pad_dict.items()}
             pad_tensor = torch.ones_like(tgt_pad_dict_[f'tgt_pad_t{time_step}'])
@@ -119,9 +120,9 @@ class PerturberMasking(CellGen):
                 prompt_length=prompt_length,
                 genes_to_perturb=genes_to_perturb,
             )
-            generate_id_dict[tgt_input_id_key] = generated_ids
-
-        return outputs, generate_id_dict
+            generate_id_dict[time_step] = generated_ids
+            all_outputs[time_step] = outputs
+        return all_outputs, generate_id_dict
 
     def generate_sequence(
         self,
@@ -236,8 +237,6 @@ class PerturberMasking(CellGen):
                 # exclude genes_to_perturb from the option to be selected
                 logits[:, :, genes_to_perturb] = max_neg_value
                 # print the shape and then check genes_to_perturb
-                print(logits.shape)
-                print('genes_to_perturb', logits[0, 0, genes_to_perturb])
             # exclude cls token
             tmp_ids_ = tmp_ids[:, prompt_length:].clone()
             scores_ = scores[:, prompt_length:].clone()
@@ -264,4 +263,4 @@ class PerturberMasking(CellGen):
             # add cls token to the ids and update scores and ids
             scores[:, prompt_length:] = scores_
             tmp_ids[:, prompt_length:] = tmp_ids_
-        return outputs, tmp_ids
+        return outputs[tgt_time_step], tmp_ids

T_perturb/Perturb/trainer.py
@@ -1,20 +1,26 @@
-from typing import Any, List
+from typing import (
+    Any,
+    List,
+    Literal,
+)
 
+import evaluate
 import torch
 
 # from geneformer.tokenizer import TOKEN_DICTIONARY_FILE
 from torch.nn.functional import cosine_similarity
 
-from T_perturb.Model.trainer import CellGenTrainer, CountDecoderTrainer
+from T_perturb.Model.trainer import CellGenTrainer
 from T_perturb.Perturb.T_model import PerturberMasking
-from T_perturb.src.utils import return_perturbation_adata  # WarmupScheduler,;
+from T_perturb.src.utils import compute_rouge_score, return_perturbation_adata
 
 
 class PerturberTrainer(CellGenTrainer):
     def __init__(
         self,
-        perturbation_mode: List[str],
         genes_to_perturb: List[int],
+        perturbation_mode: Literal['inference', 'generate'] = 'inference',
+        perturbation_sequence: Literal['src', 'tgt'] = 'src',
         perturbation_token: int = 0,
         generate: bool = False,
         sequence_length: int = 2048,
@@ -42,12 +48,12 @@ class PerturberTrainer(CellGenTrainer):
             return_attn=kwargs['return_attn'],
         )
 
-        if perturbation_mode is not None:
-            self.perturbation_mode = perturbation_mode
-            self.genes_to_perturb = torch.tensor(genes_to_perturb, dtype=torch.long)
-            self.perturbation_token = torch.tensor(
-                perturbation_token, dtype=torch.long, device=self.device
-            )
+        self.perturbation_mode = perturbation_mode
+        self.perturbation_sequence = perturbation_sequence
+        self.genes_to_perturb = torch.tensor(genes_to_perturb, dtype=torch.long)
+        self.perturbation_token = torch.tensor(
+            perturbation_token, dtype=torch.long, device=self.device
+        )
 
         self.generate = generate
         self.sequence_length = sequence_length
@@ -58,11 +64,19 @@ class PerturberTrainer(CellGenTrainer):
         self.test_dict['mean_cosine_similarity'] = []
         self.test_dict['delta_probs'] = []
 
+        self.rouge = evaluate.load('rouge')
+        self.rouge_seq_len_list = [25, 100, kwargs['max_seq_length']]
+        for seq_len in self.rouge_seq_len_list:
+            self.test_dict[f'rouge1_{seq_len}'] = []
+        self.max_seq_length = kwargs['max_seq_length']
+        self.encoder = kwargs['encoder']
+        self.pos_encoding_mode = kwargs['pos_encoding_mode']
+        self.mask_scheduler = kwargs['mask_scheduler']
+
     def forward(
         self,
         batch: Any,
         perturbation: bool = False,
-        generate: bool = False,
     ):
         if perturbation:
             self.genes_to_perturb = self.genes_to_perturb.to(self.device)
@@ -79,77 +93,43 @@ class PerturberTrainer(CellGenTrainer):
                 dim=1,
             )
             if perturbation:
-                if 'tgt' in self.perturbation_mode:
-                    print('perturbating tgt')
+                if 'tgt' in self.perturbation_sequence:
                     perturbed_tgt = tgt_input_id_.clone()
-                    print('perturbation', self.perturbation_token.device)
-                    print('perturbed_tgt', perturbed_tgt.device)
                     mask = torch.isin(tgt_input_id_, self.genes_to_perturb)
 
                     perturbed_tgt[mask] = self.perturbation_token
-                tgt_input_id_dict[f'tgt_input_ids_t{i}'] = perturbed_tgt
+                    tgt_input_id_dict[f'tgt_input_ids_t{i}'] = perturbed_tgt
+                else:
+                    tgt_input_id_dict[f'tgt_input_ids_t{i}'] = tgt_input_id_
             else:
                 tgt_input_id_dict[f'tgt_input_ids_t{i}'] = tgt_input_id_
         if perturbation:
-            if 'src' in self.perturbation_mode:
-                print('perturbating src')
+            if 'src' in self.perturbation_sequence:
                 perturbed_src = batch['src_input_ids'].clone()
                 mask = torch.isin(batch['src_input_ids'], self.genes_to_perturb)
                 perturbed_src[mask] = self.perturbation_token
+
+            else:
+                perturbed_src = batch['src_input_ids']
         else:
             perturbed_src = batch['src_input_ids']
-        if generate:
-            outputs = None
-        else:
+        if self.perturbation_mode == 'inference':
             outputs = self.transformer(
                 src_input_id=perturbed_src,
                 tgt_input_id_dict=tgt_input_id_dict,
-                not_masked=self.return_embeddings,
+                not_masked=True,
             )
+        else:
+            outputs = None
+
         return outputs, perturbed_src, tgt_input_id_dict
 
     def test_step(self, batch, *args, **kwargs):
-        if self.return_embeddings:
+        if self.perturbation_mode == 'inference':
             true_outputs, _, _ = self.forward(batch, perturbation=False)
             perturbed_outputs, _, _ = self.forward(batch, perturbation=True)
-            for t in self.pred_tps:
-                true_cls = true_outputs[t]['dec_embedding'][:, 0, :]
-                true_mean_cls = true_outputs[t]['mean_embedding']
 
-                true_logits = true_outputs[t]['dec_logits']
-                true_probs = torch.softmax(true_logits, dim=-1)
-                true_probs = true_probs.sum(dim=1)
-
-                perturbed_cls = perturbed_outputs[t]['dec_embedding'][:, 0, :]
-                perturbed_mean_cls = perturbed_outputs[t]['mean_embedding']
-                perturbed_logits = perturbed_outputs[t]['dec_logits']
-
-                perturbed_probs = torch.softmax(perturbed_logits, dim=-1)
-                perturbed_probs = perturbed_probs.sum(dim=1)
-                delta_probs = perturbed_probs - true_probs
-                delta_cls_cos_sim = cosine_similarity(
-                    perturbed_cls,
-                    true_cls,
-                )
-                delta_mean_cos_sim = cosine_similarity(
-                    perturbed_mean_cls,
-                    true_mean_cls,
-                )
-                delta_cls_cos_sim = delta_cls_cos_sim.detach().cpu()
-                delta_mean_cos_sim = delta_mean_cos_sim.detach().cpu()
-                cls_embeddings = true_cls.detach().cpu()
-                delta_probs = delta_probs.detach().cpu()
-                self.test_dict['cls_cosine_similarity'].append(delta_cls_cos_sim)
-                self.test_dict['mean_cosine_similarity'].append(delta_mean_cos_sim)
-                self.test_dict['cls_embeddings'].append(cls_embeddings)
-                self.test_dict['delta_probs'].append(delta_probs)
-
-                # return obs_key
-                self.test_dict['cell_idx'].append(batch[f'tgt_cell_idx_t{t}'])
-                if len(self.var_list) > 0:
-                    for var in self.var_list:
-                        self.test_dict[var].append(batch[f'{var}_t{t}'])
-        if self.generate:
+        elif self.perturbation_mode == 'generate':
             (
                 _,
                 pert_src_input_ids,
@@ -174,147 +154,75 @@ class PerturberTrainer(CellGenTrainer):
                 src_input_id=pert_src_input_ids,
                 **decoder_kwargs,
             )
-            for t, tgt_input_id in enumerate(true_ids_dict.keys()):
-                print('tgt_input_id', tgt_input_id)
-                print('perturbed_outputs', perturbed_outputs.keys())
+            for t in self.pred_tps:
+                pert_ids = perturbed_ids_dict[t].detach().cpu().numpy()
+                true_ids = true_ids_dict[t].detach().cpu().numpy()
 
-                pred_ids = perturbed_ids_dict[tgt_input_id].detach().cpu().numpy()
-                tgt_ids = true_ids_dict[tgt_input_id].detach().cpu().numpy()
-                # compute cosine similarity between perturbed and true
-                # t = i + 1
-                print('t', t)
-
-                # print('true_outputs', true_outputs.keys())
-                # print('true_outputs', true_outputs)
-                cls_cos_sim = cosine_similarity(
-                    perturbed_outputs[t]['dec_embedding'][:, 0, :],
-                    true_outputs[t]['dec_embedding'][:, 0, :],
+                test_dict = compute_rouge_score(
+                    rouge=self.rouge,
+                    pred_ids=pert_ids,
+                    tgt_ids=true_ids,
+                    rouge_len_list=self.rouge_seq_len_list,
+                    max_seq_length=self.max_seq_length,
+                    test_dict=self.test_dict,
                 )
-                mean_agg_cos_sim = cosine_similarity(
-                    perturbed_outputs[t]['mean_embedding'],
-                    true_outputs[t]['mean_embedding'],
-                )
-                self.test_dict['cls_cosine_similarity'].append(cls_cos_sim)
-                self.test_dict['mean_cosine_similarity'].append(mean_agg_cos_sim)
-
-                if self.return_rouge_score:
-                    test_dict = self.compute_rouge_score(
-                        pred_ids=pred_ids,
-                        tgt_ids=tgt_ids,
-                        rouge_len_list=self.rouge_seq_len_list,
-                        max_seq_length=self.max_seq_length,
-                        test_dict=self.test_dict,
-                    )
-                    self.test_dict = test_dict
-                    # self.log(
-                    #     'test/rouge1',
-                    #     rouge_score['rouge1'],
-                    #     on_step=False,
-                    #     on_epoch=True,
-                    #     prog_bar=True,
-                    #     logger=True,
-                    #     rank_zero_only=True,
-                    #     sync_dist=True,
-                    #     batch_size=batch['src_input_ids'].shape[0],
-                    # )
-            for time_step in self.pred_tps:
-                self.test_dict['cell_idx'].append(batch[f'tgt_cell_idx_t{time_step}'])
-                if len(self.var_list) > 0:
-                    for var in self.var_list:
-                        self.test_dict[var].append(batch[f'{var}_t{time_step}'])
-                cls_embeddings = true_outputs[time_step]['cls_embedding'].detach().cpu()
-                self.test_dict['cls_embeddings'].append(cls_embeddings)
-
-    def on_test_epoch_end(self):
-        if self.return_embeddings:
-            obs_key = self.var_list if len(self.var_list) > 0 else []
-            obs_key.extend(['cell_idx'])
-            return_perturbation_adata(
-                test_dict=self.test_dict,
-                obs_key=obs_key,
-                output_dir=self.output_dir,
-                file_name=(
-                    f'{self.date}_prediction_embeddings'
-                    f't{self.pred_tps}_lr{self.end_lr}_w{self.weight_decay}'
-                ),
-                mode='inference',
-            )
-
-
-class PerturberGenerationTrainer(CountDecoderTrainer):
-    def __init__(
-        self,
-        genes_to_perturb: List[int] | None = None,
-        perturbation_token: int | None = 0,
-        cell_type_to_perturb: str | None = None,
-        perturbation_mode: List[str] | None = None,
-        *args,
-        **kwargs,
-    ):
-        super().__init__(*args, **kwargs)
-        print('test kwargs', kwargs)
-
-        self.decoder = PerturberMasking(
-            pretrained_model=self.pretrained_model,
-            loss_mode=kwargs['loss_mode'],
-            d_model=kwargs['d_model'],
-            dropout=kwargs['dropout'],
-            pred_tps=kwargs['pred_tps'],
-            context_tps=kwargs['context_tps'],
-            context_mode=kwargs['context_mode'],
-            n_genes=kwargs['n_genes'],
-        )
-        if perturbation_mode is not None:
-            self.perturbation_mode = perturbation_mode
-            self.genes_to_perturb = torch.tensor(genes_to_perturb, dtype=torch.long)
-            self.perturbation_token = torch.tensor(perturbation_token, dtype=torch.long)
+                self.test_dict = test_dict
         else:
-            self.perturbation_mode = []
-        self.test_dict['cls_cosine_similarity'] = []
-        self.test_dict['mean_cosine_similarity'] = []
-
-    def test_step(self, batch, *args, **kwargs):
-        tgt_input_id_dict = {}
-        for i in self.total_tps:
-            print(i)
-            tgt_input_id_ = torch.cat(
-                (
-                    getattr(self, f'cls_token_{str(i)}').expand(
-                        batch[f'tgt_input_ids_t{i}'].shape[0], -1
-                    ),
-                    batch[f'tgt_input_ids_t{i}'],
-                ),
-                dim=1,
+            raise ValueError(
+                f'Invalid perturbation mode: {self.perturbation_mode}:'
+                f'Choose between "inference" or "generate"'
             )
-            tgt_input_id_dict[f'tgt_input_ids_t{i}'] = tgt_input_id_
-            if len(self.perturbation_mode) > 0:
-                if 'tgt' in self.perturbation_mode:
-                    print('perturbating tgt')
-                    perturbed_tgt = batch[f'tgt_input_ids_t{i}'].clone()
-                    mask = torch.isin(
-                        batch[f'tgt_input_ids_t{i}'], self.genes_to_perturb
-                    )
-                    perturbed_tgt[mask] = self.perturbation_token
-        if len(self.perturbation_mode) > 0:
-            if 'src' in self.perturbation_mode:
-                print('perturbating src')
-                perturbed_src = batch['src_input_ids'].clone()
-                mask = torch.isin(batch['src_input_ids'], self.genes_to_perturb)
-                perturbed_src[mask] = self.perturbation_token
+        for t in self.pred_tps:
+            true_cls = true_outputs[t]['dec_embedding'][:, 0, :]
+            true_mean_cls = true_outputs[t]['mean_embedding']
+
+            true_logits = true_outputs[t]['dec_logits']
+            true_probs = torch.softmax(true_logits, dim=-1)
+            true_probs = true_probs.sum(dim=1)
+
+            perturbed_cls = perturbed_outputs[t]['dec_embedding'][:, 0, :]
+            perturbed_mean_cls = perturbed_outputs[t]['mean_embedding']
+            perturbed_logits = perturbed_outputs[t]['dec_logits']
+
+            perturbed_probs = torch.softmax(perturbed_logits, dim=-1)
+            perturbed_probs = perturbed_probs.sum(dim=1)
+            delta_probs = perturbed_probs - true_probs
+            delta_cls_cos_sim = cosine_similarity(
+                perturbed_cls,
+                true_cls,
+            )
+            delta_mean_cos_sim = cosine_similarity(
+                perturbed_mean_cls,
+                true_mean_cls,
+            )
+            delta_cls_cos_sim = delta_cls_cos_sim.detach().cpu()
+            delta_mean_cos_sim = delta_mean_cos_sim.detach().cpu()
+            cls_embeddings = true_cls.detach().cpu()
+            delta_probs = delta_probs.detach().cpu()
+            self.test_dict['cls_cosine_similarity'].append(delta_cls_cos_sim)
+            self.test_dict['mean_cosine_similarity'].append(delta_mean_cos_sim)
+            self.test_dict['cls_embeddings'].append(cls_embeddings)
+            self.test_dict['delta_probs'].append(delta_probs)
+
+            # return obs_key
+            self.test_dict['cell_idx'].append(batch[f'tgt_cell_idx_t{t}'])
+            if len(self.var_list) > 0:
+                for var in self.var_list:
+                    self.test_dict[var].append(batch[f'{var}_t{t}'])
+            print('test_dict', self.test_dict)
 
     def on_test_epoch_end(self):
-        if self.generate:
-            obs_key = self.var_list if len(self.var_list) > 0 else []
-            obs_key.extend(['cell_idx'])
-            return_perturbation_adata(
-                test_dict=self.test_dict,
-                obs_key=obs_key,
-                output_dir=self.output_dir,
-                file_name=(
-                    f'{self.date}_generate_adata_'
-                    f't{self.pred_tps}_{self.encoder}_s{self.seed}_'
-                    f'l{self.loss_mode}_n{self.n_samples}'
-                    f'_p{self.pos_encoding_mode}_'
-                    f'm{self.mask_scheduler}_s{self.sequence_length}'
-                ),
-            )
+        obs_key = self.var_list if len(self.var_list) > 0 else []
+        obs_key.extend(['cell_idx'])
+        return_perturbation_adata(
+            test_dict=self.test_dict,
+            obs_key=obs_key,
+            output_dir=self.output_dir,
+            file_name=(
+                f'{self.date}_{self.perturbation_mode}_adata_'
+                f't{self.pred_tps}_{self.encoder}'
+                f'_p{self.pos_encoding_mode}_'
+                f'm{self.mask_scheduler}_s{self.sequence_length}'
+            ),
+            mode=self.perturbation_mode,
+        )

T_perturb/src/utils.py
@@ -199,6 +199,65 @@ def condition_for_count_loss(
     )
 
 
+def compute_rouge_score(
+    rouge,
+    pred_ids: np.ndarray,
+    tgt_ids: np.ndarray,
+    rouge_len_list: list[int],
+    max_seq_length: int,
+    test_dict: dict[str, list],
+) -> dict[str, list[float]]:
+    # Convert predictions and targets to object type for string replacement
+    pred_ids = pred_ids.astype(object)
+    tgt_ids = tgt_ids.astype(object)
+
+    # Exclude task token (assumed to be the first token)
+    pred_ids = pred_ids[:, 1:]
+
+    # Define special tokens to remove (e.g., 0, 1, 2, 3)
+    special_tokens = np.array([0, 1, 2, 3])
+    pred_ids[np.isin(pred_ids, special_tokens)] = ''
+    tgt_ids[np.isin(tgt_ids, special_tokens)] = ''
+
+    # Convert all integers to strings
+    pred_ids_ = pred_ids.astype(str)
+    tgt_ids_ = tgt_ids.astype(str)
+
+    for seq_len in rouge_len_list:
+        # Truncate sequences based on seq_len
+        if max_seq_length > seq_len:
+            pred_genes_short = pred_ids_[:, :seq_len]
+            true_genes_short = tgt_ids_[:, :seq_len]
+        else:
+            pred_genes_short = pred_ids_
+            true_genes_short = tgt_ids_
+
+        # Convert each row of token IDs to a string
+        pred_ids_str = np.apply_along_axis(
+            lambda row: ' '.join(row), axis=1, arr=pred_genes_short
+        )
+        tgt_ids_str = np.apply_along_axis(
+            lambda row: ' '.join(row), axis=1, arr=true_genes_short
+        )
+
+        # Remove extra spaces
+        pred_ids_str = np.array([' '.join(s.split()) for s in pred_ids_str])
+        tgt_ids_str = np.array([' '.join(s.split()) for s in tgt_ids_str])
+        # Compute ROUGE score for the entire batch at once
+        rouge_result = rouge.compute(
+            predictions=pred_ids_str.tolist(),
+            references=tgt_ids_str.tolist(),
+            rouge_types=['rouge1'],
+            use_aggregator=False,
+        )
+        # Extract the per-sample ROUGE scores
+        per_sample_scores = rouge_result['rouge1']
+        # Store the results in the test_dict
+        test_dict[f'rouge1_{seq_len}'].append(per_sample_scores)
+
+    return test_dict
+
+
 def compute_cos_similarity(
     outputs: dict,
     time_step: int,
@@ -714,7 +773,7 @@ def return_perturbation_adata(
     obs_key: list,
     output_dir: str,
     file_name: str,
-    mode: Literal['inference', 'generation'],
+    mode: Literal['inference', 'generate'],
 ) -> ad.AnnData:
     """
     Description:
@@ -755,29 +814,23 @@ def return_perturbation_adata(
     cls_embeddings = torch.cat(test_dict['cls_embeddings']).numpy()
     cls_cos_similarity = torch.cat(test_dict['cls_cosine_similarity']).numpy()
     mean_cos_similarity = torch.cat(test_dict['mean_cosine_similarity']).numpy()
+    delta_probs = torch.cat(test_dict['delta_probs']).numpy()
     print('cosine_similarity', mean_cos_similarity.shape)
     # create dataframe to store perturbation results
     obsm_dict = {
         'cls_embeddings': cls_embeddings,
         'cls_cos_similarity': cls_cos_similarity,
         'mean_cos_similarity': mean_cos_similarity,
+        'delta_probs': delta_probs,
     }
-    if mode == 'generation':
+    if mode == 'generate':
+        print(test_dict['rouge1_25'])
         rouge_dict = {
             key: np.concatenate(test_dict[key])
             for key in test_dict.keys()
             if key.startswith('rouge')
         }
         obsm_dict.update(rouge_dict)
-    elif mode == 'inference':
-        print(test_dict['delta_probs'])
-        delta_probs = torch.cat(test_dict['delta_probs']).numpy()
-        print('delta', delta_probs.shape)
-        obsm_dict['delta_probs'] = delta_probs
-    else:
-        raise ValueError(
-            f'Invalid mode: {mode}.' 'Must be either generation or inference.'
-        )
 
     # adata.obs
     obs_dict = {obs: np.concatenate(test_dict[obs]) for obs in obs_key}
@@ -787,7 +840,7 @@ def return_perturbation_adata(
         obs=test_obs,
         obsm=obsm_dict,
     )
-    # adata.write_h5ad(os.path.join(output_dir, file_name))
+    adata.write_h5ad(os.path.join(output_dir, file_name))
     print('anndata generation completed---')
     return adata
 