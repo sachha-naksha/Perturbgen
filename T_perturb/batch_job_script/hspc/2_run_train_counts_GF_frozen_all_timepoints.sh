38e734231bb43682bb70aaa0e94b8b2dc0bf090c
kchl5 <k.ly@hotmail.ch>
2025-04-08 14:56:26 +0100

    added script for run the plotting of pert res

--- T_perturb/batch_job_script/hspc/2_run_train_counts_GF_frozen_all_timepoints.sh
+++ T_perturb/batch_job_script/hspc/2_run_train_counts_GF_frozen_all_timepoints.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #BSUB -q gpu-lotfollahi # name of the partition to run job on (options: gpu-normal, gpu-huge, gpu-lotfollahi)
-#BSUB -gpu 'mode=exclusive_process:num=3:block=yes' # request for exclusive access to gpu :gmodel=NVIDIAA100_SXM4_80GB
+#BSUB -gpu 'mode=exclusive_process:num=4:block=yes' # request for exclusive access to gpu :gmodel=NVIDIAA100_SXM4_80GB
 #BSUB -n 4 # number of cores
 #BSUB -G team361 # groupname for billing
 #BSUB -cwd /lustre/scratch126/cellgen/team361/kl11/t_generative/T_perturb/T_perturb # working directory
---
+++ T_perturb/batch_job_script/hspc/4_run_plotting_perturb_res.sh
@@ -0,0 +1,23 @@
+#make a date directory if it does not exist
+#!/bin/bash
+#BSUB -q normal # run CPU job
+#BSUB -n 4 # number of cores
+#BSUB -G team361 # groupname for billing
+#BSUB -cwd /lustre/scratch126/cellgen/team361/kl11/t_generative/ # working directory
+#BSUB -o T_perturb/T_perturb/logs/plt_perturb_res_RUNX1_%J.out # output file
+#BSUB -e T_perturb/T_perturb/logs/plt_perturb_res_RUNX1_%J.err # error file
+#BSUB -M 50000  # RAM memory part 2. Default: 100MB
+#BSUB -R 'select[mem>50000] rusage[mem=50000]' # RAM memory part 1. Default: 100MB
+#BSUB -J plt_perturb_res_RUNX1 # job name
+
+# activate python environment
+source /lustre/scratch126/cellgen/team361/kl11/t_generative/.cellgen_4096/bin/activate
+cwd=$(pwd)
+
+echo "--- Start plotting"
+
+python3 $cwd/TRACE-reproducibility/HSPC/2.2.1_multiple_perturbation.py \
+--perturbed_gene 'RUNX1' \
+--p_perturbation 'T_perturb/T_perturb/plt/res/hspc/pbmc_median/perturbation/20250407-20:37_minference_adata_gRUNX1_ssrc_tmask.h5ad'
+
+echo "--- Finished plotting"
